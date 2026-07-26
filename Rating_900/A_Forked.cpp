d2fa8d2a0c913a4a7a82e349a3b01d1de665a406
Md Abu Rayhan <rayhan19122@gmail.com>
2026-07-27 01:02:15 +0600

    fucking forked added

--- Rating_900/A_Forked.cpp
+++ Rating_900/A_Forked.cpp
@@ -7,6 +7,30 @@ using namespace std;
 void solve() {
     int a,b;
     cin>>a>>b;
+    int k_x,k_y,q_x,q_y;
+    cin>>k_x>>k_y;
+    cin>>q_x>>q_y;
+
+    set<pair<int,int>>king,queen;
+
+    int dx[4]={1,-1,1,-1}, dy[4]={1,-1,-1,1};
+
+    for (int i = 0; i < 4; i++)
+    {
+        king.insert({k_x+a*dx[i],k_y+b*dy[i]});
+        king.insert({k_x+b*dx[i],k_y+a*dy[i]});
+        
+        queen.insert({q_x+b*dx[i],q_y+a*dy[i]});
+        queen.insert({q_x+a*dx[i],q_y+b*dy[i]});
+
+    }
+    int ans=0;
+    for (auto x:king)
+    {
+        if(queen.find(x)!=queen.end()) ans++;
+    }
+    cout<<ans<<endl;
+    
 }
 
 signed main() {
