5a1639e02cb66f61534f801a820b811a87812526
Md Abu Rayhan <rayhan19122@gmail.com>
2026-07-31 12:14:18 +0600

    B_Chemistry added

--- Rating_900/B_Chemistry.cpp
+++ Rating_900/B_Chemistry.cpp
@@ -5,7 +5,22 @@ using namespace std;
 #define endl '\n'
 
 void solve() {
+    int n,k;
+    cin>>n>>k;
+
+    string s;
+    cin>>s;
+
+    map<char,int>mp;
+    for(auto u:s) mp[u]++;
     
+    int odd_freq=0; int even_freq=0;
+    for(auto u:mp){
+        if(u.second%2) odd_freq++;
+        else even_freq++;
+    }
+    if(odd_freq>k+1) cout<<"NO"<<endl;
+    else cout<<"YES"<<endl;
 }
 
 signed main() {
